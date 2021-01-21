f70f490b3737d7289d1995413782e04d3099da36
mdrokz <mohammadmunshi@gmail.com>
2021-01-22 01:18:41 +0530

    header file update

--- src/binarytreealgorithm.h
+++ src/binarytreealgorithm.h
@@ -7,7 +7,6 @@
 #ifndef BINARYTREEALGORTHM_H
 #define BINARYTREEALGORTHM_H
 
-
 //==============================================================================
 //! Node Struct
 //! Node Is A Node Of A Binary Tree
@@ -22,9 +21,7 @@ struct Node
     //! @param aHeight The Height Of The Rectangle
     Node(int aX, int aY, int aWidth, int aHeight)
         : x(aX), y(aY), width(aWidth), height(aHeight), isUsed(false),
-        imgID(-1), rightChild(nullptr), downChild(nullptr)
-    {
-    };
+          imgID(-1), rightChild(nullptr), downChild(nullptr){};
 
     // the top-left coordinate of the rectangle
     int x;
@@ -40,7 +37,7 @@ struct Node
     int imgID;
 
     // after placing an image on the top left corner of an empty rectangular
-    // the remaining space will be splitted into 2 rectangles by the    
+    // the remaining space will be splitted into 2 rectangles by the
     // horizontal line where the image's bottom line is
     // ---------------------
     // |  img | rightChild |
@@ -50,17 +47,16 @@ struct Node
     // |                   |
     // |                   |
     // ---------------------
-    Node* rightChild;
-    Node* downChild;
+    Node *rightChild;
+    Node *downChild;
 };
-
 
 //==============================================================================
 //! BinaryTreeAlgorithm Class
 //==============================================================================
 class BinaryTreeAlgorithm
 {
-    public:
+public:
     //! @brief Starts A New Packing Process To A Texture Atlas Canvas Of The Given Dimension
     //! @param aAtlasWidth The Atlas Canvas's Width
     //! @param aAtlasHeight The Atlas Canvas's Height
@@ -71,60 +67,68 @@ class BinaryTreeAlgorithm
     //! @param aImgHeight The Image's Height
     //! @return The Pointer To The Node That Stores The Image
     //!         or nullptr If It Didn't Fit In The Binary Tree
-    Node* Insert(const int aImgWidth, const int aImgHeight) const;
+    Node *Insert(const int aImgWidth, const int aImgHeight) const;
 
-    //! @brief SplitNode Happens Every Time After Placing An Image 
+    //! @brief SplitNode Happens Every Time After Placing An Image
     //         On The Top Left Corner Of An Empty Rectangular
-    //! @param aNode The Node Where The Image Just Inserted  
+    //! @param aNode The Node Where The Image Just Inserted
     //! @param aImgWidth The Image's Width
     //! @param aImgHeight The Image's Height
     //! @param aImgID The Corresponding Index Of The Image In iSortedImageList Is Used As aImgID
-    void SplitNode(Node* aNode, const int aImgWidth, const int aImgHeight, const int aImgID);
+    void SplitNode(Node *aNode, const int aImgWidth, const int aImgHeight, const int aImgID);
 
     //! @brief GrowAtlasCanvas Provides An Algorithm Which Decides Where The Canvas Should Expend,
     //         On Its Right Side by Call GrowRight Method Or On Its Down Side by Call GrowDown Method
     //! @param aImgWidth The Image's Width
     //! @param aImgHeight The Image's Height
     //! @param aImgID The Corresponding Index Of The Image In iSortedImageList Is Used As aImgID
-    Node*  GrowAtlasCanvas(const int aImgWidth, const int aImgHeight, const int aImgID);
+    Node *GrowAtlasCanvas(const int aImgWidth, const int aImgHeight, const int aImgID);
 
     //! @brief GrowAtlasCanvas Grows Canvas On Right Side
     //! @param aImgWidth The Image's Width
     //! @param aImgHeight The Image's Height
     //! @param aImgID The Corresponding Index Of The Image In iSortedImageList Is Used As aImgID
     //! @return The Pointer To The Node That Stores The Newly Added Image
-    Node*  GrowRight(int aWide, int aHeight, const int aImgID);
+    Node *GrowRight(int aWide, int aHeight, const int aImgID);
 
     //! @brief GrowAtlasCanvas Grows Canvas On Down Side
     //! @param aImgWidth The Image's Width
     //! @param aImgHeight The Image's Height
     //! @param aImgID The Corresponding Index Of The Image In iSortedImageList Is Used As aImgID
     //! @return The Pointer To The Node That Stores The Newly Added Image
-    Node*  GrowDown(int aWide, int aHeight, const int aImgID);
+    Node *GrowDown(int aWide, int aHeight, const int aImgID);
 
-    int DownChildLength(int count, Node* node);
+    //! @brief DownChildLength returns row length
+    //! @param count count should be 0 at start
+    //! @param node rootNode 
+    //! @return Rows in the tree
+    int DownChildLength(int count, Node *node);
 
-    int RightChildLength(int count, Node* node);
+    //! @brief RightChildLength returns column length
+    //! @param count count should be 0 at start
+    //! @param node rootNode 
+    //! @return Columns in the tree
+    int RightChildLength(int count, Node *node);
 
     //! @brief Get the rootNode
-    Node*  rootNode()
+    Node *rootNode()
     {
         return iRootNode;
     };
 
-    private:
+private:
     //! @brief Inserts An Image In The Subtree Rooted At The Given Node
     //! @param aNode The Given Node Where To Insert The Image
     //! @param aImgWidth The The Image's Width
     //! @param aImgHeight The The Image's Height
     //! @return The Pointer To The Node That Stores The Newly Added Image
     //!         or nullptr If It Didn't Fit In The Binary Tree
-    Node* Insert(Node* aNode, const int aImgWidth, const int aImgHeight) const;
+    Node *Insert(Node *aNode, const int aImgWidth, const int aImgHeight) const;
 
-    private:
-    Node* iRootNode;
+private:
+    Node *iRootNode;
 };
 
-#endif    // BINARYTREEALGORTHM_H
+#endif // BINARYTREEALGORTHM_H
 
 // End Of File
